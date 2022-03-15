a900bf5d6e3782e99c9245dd6ef30da6edd20fbd
Manali Darji <darjimanali93@gmail.com>
2022-03-15 18:43:04 -0400

    add basic files for new custom theme

---
+++ wp-content/themes/custom-blog-theme/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php bloginfo('title'); ?></title>
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<meta name="description" content="<?php bloginfo('description'); ?>">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+	<header class="header">
+		header	    
+	</header>
